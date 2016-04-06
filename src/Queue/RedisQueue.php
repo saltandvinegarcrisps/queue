85f94491aed149f0bed2875d1406bbdc714c2aa8
Kieron <info@kieronwilson.co.uk>
2016-04-06 11:42:30 +0100

    added redis

---
+++ src/Queue/RedisQueue.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Queue;
+
+class RedisQueue implements MessageQueue, \Countable {
+
+	protected $redis;
+
+	protected $channel;
+
+	public function __construct(\Redis $redis, $channel = 'default') {
+		$this->redis = $redis;
+		$this->channel = $channel;
+	}
+
+	public function count() {
+		return $this->redis->lSize('queue:' . $this->channel);
+	}
+
+	public function push($message) {
+		// append to the end (right)
+		$this->redis->rPush('queue:' . $this->channel, $message);
+	}
+
+	public function pop() {
+		// pop from the start (left)
+		return $this->redis->lPop('queue:' . $this->channel);
+	}
+
+}
